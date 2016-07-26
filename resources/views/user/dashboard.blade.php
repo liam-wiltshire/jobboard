7f787cfddd5e96b2eba5818db3d87e6d6554be76
Liam Wiltshire <liam@lswlaptop.local>
2016-07-26 21:05:17 +0100

    Tidying up user dashboard

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -17,7 +17,19 @@
                     <div class="panel-body">
                         <div class="row">
                             @foreach ($jobs as $job)
-                                <div class="col-sm-6 col-md-4"><h3 style="height:40px">{{$job->job}}</h3><p>{{substr($job->description,0,55)}}...</p><p style="font-weight:bold;">Price: &pound;{{number_format($job->price,2)}}</p><hr /><a href="/user/job/{{$job->id}}" class="btn btn-success">View/Claim</a></h3></div>
+                                <div class="col-sm-6 col-md-4">
+                                    <div class="panel panel-info">
+                                        <div class="panel-heading">
+                                            {{$job->job}}
+                                        </div>
+                                        <div class="panel-body">
+                                            <p>{{substr($job->description,0,55)}}...</p>
+                                            <p>Price: &pound;{{number_format($job->price,2)}}</p>
+                                        </div>
+                                        <div class="panel-footer">
+                                            <a href="/user/job/{{$job->id}}" class="btn btn-success">View/Claim</a>
+                                        </div>
+                                    </div>
                             @endforeach
                         </div>
                     </div>
